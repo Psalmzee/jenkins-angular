ae4779797b9f814e8c4e8d791ce16cc60f96fc1c
Samson Okeji <engrsamsonokeji@gmail.com>
2023-07-30 15:39:20 +0100

    main commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+const routes = [
+  { path: '', component: HomeComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    RouterModule.forRoot(routes) // Define the routes using RouterModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
